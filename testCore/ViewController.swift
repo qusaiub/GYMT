312e37950d2013423e5edcb7d7a0a3e2cfbd1d6d
HackerU <qusaiasaad@qusais-macbook-pro.local>
2017-01-23 21:08:17 +0200

    Initial Commit

---
+++ testCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testCore
+//
+//  Created by Qusai Asaad on 23.1.2017.
+//  Copyright © 2017 Qusai Asaad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
